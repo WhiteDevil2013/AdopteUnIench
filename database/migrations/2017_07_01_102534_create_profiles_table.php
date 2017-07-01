6d867f17e34da5e79428c9139f994353e3625f20
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-07-01 12:32:10 +0200

    Add profile table

---
+++ database/migrations/2017_07_01_102534_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->boolean('isAnimal');
+            $table->string('race');
+            $table->string('description');
+            $table->date('birthDate');
+            $table->string('location');
+            $table->string('profilePicture');
+            $table->boolean('sex');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
