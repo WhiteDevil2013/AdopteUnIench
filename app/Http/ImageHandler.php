fc5bc662134958294e0ac6c3b80e28975c6b4667
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-07-01 17:45:14 +0200

    Upload image -- to fix

---
+++ app/Http/ImageHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AdopteUnIench\Http;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lorris
+ * Date: 01/07/2017
+ * Time: 16:58
+ */
+class ImageHandler
+{
+    private function resizeImage($fileToResize)
+    {
+        list($orig_width, $orig_height) = getimagesize($fileToResize);
+
+        $width = $orig_width;
+        $height = $orig_height;
+
+        $dst_image = imagecreatetruecolor(420, 420);
+        $type = mime_content_type($fileToResize);
+        $src_image = null;
+        switch (substr($type, 6)) {
+            case 'jpeg':
+                $src_image = imagecreatefromjpeg($fileToResize);
+                break;
+            case 'gif':
+                $src_image = imagecreatefromgif($fileToResize);
+                break;
+            case 'png':
+                $src_image = imagecreatefrompng($fileToResize);
+                break;
+            case 'bmp':
+                $src_image = imagecreatefromwbmp($fileToResize);
+                break;
+        }
+        imagecopyresampled($dst_image, $src_image, 0, 0, 0, 0, 420, 420, $width, $height);
+
+        imagejpeg($dst_image, $fileToResize, 100);
+    }
+
+    public function uploadImageOnDisk($fileToUpload)
+    {
+        $this->resizeImage($fileToUpload['tmp_name']);
+        $imageData = addslashes($fileToUpload['tmp_name']);
+        $imageData = file_get_contents($imageData);
+        $imageName = addslashes($fileToUpload['name']);
+        $location = __DIR__.'/images/' . $imageName;
+        file_put_contents($location, $imageData);
+        return $location;
+    }
+}
