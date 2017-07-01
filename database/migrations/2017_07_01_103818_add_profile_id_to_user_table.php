ecf0a0c9c63e2c5fb335d06bfce00abe14fee418
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-07-01 12:49:39 +0200

    Add profile_id to user table

---
+++ database/migrations/2017_07_01_103818_add_profile_id_to_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProfileIdToUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('profile_id')->unsigned();
+            $table->foreign('profile_id')->references('id')->on('profiles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_profile_id_foreign');
+        });
+    }
+}
