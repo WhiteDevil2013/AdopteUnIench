d9ada841e8f0634beb6a237ac75a9c39c4c5ee3c
Moussa Oufkir <moussa.oufkir@epita.fr>
2017-07-02 14:04:03 +0200

    [home] basic home profile list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace AdopteUnIench\Http\Controllers;
 
 use Illuminate\Http\Request;
+use AdopteUnIench\Profile;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', ['profiles' => Profile::all()]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,14 @@
 
                 <div class="panel-body">
                     Vous êtes maintenant connecté !
+
+                    <h3>Ces profils peuvent vous interesser :</h3>
+                    @foreach ($profiles as $profile)
+                    <p>{{ $profile->username }} : {{ $profile->race}}</p>
+                    <p>{{ $profile->description }}</p>
+                    <p>Habite à {{ $profile->location }}</p>
+                    <br />
+                    @endforeach
                 </div>
             </div>
         </div>
