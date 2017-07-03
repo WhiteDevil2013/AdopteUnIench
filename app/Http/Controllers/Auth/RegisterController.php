dbfd0f3ed6103305e67d589b154d942bb1bdb224
Lorris Saint-Genez <lorris.saint-genez@epita.fr>
2017-07-03 09:44:33 +0200

    Fix sign up fields (#7)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
             'username' => 'required|string|unique:profiles',
